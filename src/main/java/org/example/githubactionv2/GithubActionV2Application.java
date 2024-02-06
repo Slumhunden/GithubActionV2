85d7da8ee33cc35e3b9722559987a0816534d996
Slumhunden <kroghbastin@gmail.com>
2024-02-06 09:53:53 +0100

    start

---
+++ src/main/java/org/example/githubactionv2/GithubActionV2Application.java
@@ -0,0 +1,13 @@
+package org.example.githubactionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubactionv2/GithubActionV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
