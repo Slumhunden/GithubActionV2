f7ac8654f435b632095ca435964f3d10e2c32ccd
Slumhunden <kroghbastin@gmail.com>
2024-02-06 10:12:40 +0100

    hello world

--- src/main/java/org/example/githubactionv2/GithubActionV2Application.java
+++ src/main/java/org/example/githubactionv2/GithubActionV2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionV2Application {
 
     public static void main(String[] args) {
+        System.out.println("hej");
         SpringApplication.run(GithubActionV2Application.class, args);
     }
 
